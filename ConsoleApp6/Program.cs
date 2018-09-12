886128fdf8b6566d579f3247c32449ed27686d46
dan nguyen <ddnguyen0702@hotmail.com>
2018-09-12 11:58:10 +0700

    Add comment

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,7 +1,19 @@
 ﻿using System;
 
+    //Cryptographic primitive | Hash |    MAC    | Digital
+    //Security Goal           |      |           | signature
+    //------------------------+------+-----------+-------------
+    //Integrity               |  Yes |    Yes    |   Yes
+    //Authentication          |  No  |    Yes    |   Yes
+    //Non-repudiation         |  No  |    No     |   Yes
+    //------------------------+------+-----------+-------------
+    //Kind of keys            | none | symmetric | asymmetric
+    //                        |      |    keys   |    keys
+
+
 namespace MacIdeaDemo
 {
+
     class Text
     {
         public string Name { get; set; }

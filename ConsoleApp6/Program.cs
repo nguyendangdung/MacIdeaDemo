f267190f779acc4efc47768bf4e2059aac3517af
dan nguyen <ddnguyen0702@hotmail.com>
2018-09-12 11:54:02 +0700

    INit

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,15 +1,109 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp6
+namespace MacIdeaDemo
 {
+    class Text
+    {
+        public string Name { get; set; }
+    }
+
+    class Key
+    {
+        public string Value { get; set; }
+    }
+
+    class MacTag
+    {
+        public string Value { get; set; }
+    }
+    class Block
+    {
+        public Text Text { get; set; }
+        public Key Key { get; set; }
+
+        /// <summary>
+        /// using an Hash function to return MacTag
+        /// </summary>
+        /// <returns></returns>
+        public MacTag GetMacTag()
+        {
+            return new MacTag()
+            {
+                Value = Text.Name + Key.Value
+            };
+        }
+    }
+
+
+    
+
     class Program
     {
         static void Main(string[] args)
         {
+            string secretkey = "123";
+            var originText = new Text()
+            {
+                Name = "dan nguyen"
+            };
+            var key = new Key()
+            {
+                Value = secretkey
+            };
+
+            var block = new Block()
+            {
+                Text = originText,
+                Key = key
+            };
+
+            var originMac = block.GetMacTag();
+
+            // 
+            var backData = SendViaInternet(originText, originMac);
+
+            var textback = (Text)backData.Text;
+            var tagBack = (MacTag)backData.MacTag;
+
+
+            var blockback = new Block()
+            {
+                Text = textback,
+                Key = key
+            };
+
+            if (blockback.GetMacTag().Value != tagBack.Value)
+            {
+                Console.WriteLine("Fake data");
+            }
+            else
+            {
+                Console.WriteLine("Good data");
+            }
+
+
+        }
+
+
+        private static dynamic SendViaInternet(Text origin, MacTag orMacTag)
+        {
+            var fakeText = new Text()
+            {
+                Name = "xxxxx"
+            };
+            var fakeBlock = new Block()
+            {
+                Text = fakeText,
+                Key = new Key()
+                {
+                    Value = "00000000000"
+                }
+            };
+            return new
+            {
+                Text = fakeText,
+                MacTag = fakeBlock.GetMacTag()
+            };
         }
     }
 }
